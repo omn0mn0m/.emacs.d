03cac8bed5d326d33871bb50a56e1d8c61b383da
Nam Tran <tranngocnam97@yahoo.com>
2017-09-14 04:05:27 -0400

    Added mostly cosmetic changes

--- init.el
+++ init.el
@@ -1,9 +1,19 @@
 ;; ---------------------------------------------------------------------------
-;; Activates Melpa package archive
+;; Package Stuff
 ;; ---------------------------------------------------------------------------
+(package-initialize)
+
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+(unless (assoc-default "org" package-archives)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa") t))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+(require 'use-package)
 
 ;; ---------------------------------------------------------------------------
 ;; Cosmetic changes
@@ -12,6 +22,29 @@
 (load-theme 'dracula t)
 
 (tool-bar-mode -1)
+
+;; Mode Line Changes
+(use-package smart-mode-line)
+(use-package smart-mode-line-powerline-theme)
+(setq sml/theme 'powerline)
+(setq sml/no-confirm-load-theme t)  ;; Workaround to always asking for load
+(sml/setup)
+
+;; Dired
+(use-package dired-details)
+(setq-default dired-details-hidden-string "--- ")
+(dired-details-install)
+
+;; ---------------------------------------------------------------------------
+;; Programming Stuff
+;; ---------------------------------------------------------------------------
+
+;; Octave
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+
+;; Magit
+(use-package magit)
 
 ;; ---------------------------------------------------------------------------
 ;; Default init.el config
@@ -25,11 +58,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit)))
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (dired-details miniedit smart-mode-line-powerline-theme smart-mode-line auto-compile use-package magit)))
  '(python-shell-completion-native-enable nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
