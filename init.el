6f6de44c56b7dfec0effdf28fea90cf58b0ff755
Nam Tran <omn0mn0m@protonmail.com>
2021-04-14 19:25:12 -0400

    Disabled electric-indent and enabled fido-mode

--- init.el
+++ init.el
@@ -58,11 +58,14 @@ Inhibits startup screen on the first unrecognised option."
 ;; Replaces yes or no with y or n shorthands
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Sentences end with a single space
+(setq sentence-end-double-space nil)
+
 ;; Replaces marked region with what is being typed
 (delete-selection-mode 1)
 
-;; Sentences end with a single space
-(setq sentence-end-double-space nil)
+;; Turn off tabs
+(setq-default indent-tabs-mode nil)
 
 ;; Projectile
 (use-package projectile
@@ -76,6 +79,9 @@ Inhibits startup screen on the first unrecognised option."
 (use-package miniedit
   :commands minibuffer-edit
   :init (miniedit-install))
+
+;; Fido Mode
+(fido-mode)
 
 ;; ---------------------------------------------------------------------------
 ;; Programming Stuff
@@ -102,8 +108,19 @@ Inhibits startup screen on the first unrecognised option."
 ;; Ren'Py
 (load "renpy")
 
-;; Elixir
+;; Web Dev
 (use-package elixir-mode)
+
+(use-package web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(use-package pug-mode)
 
 ;; PlatformIO
 (use-package platformio-mode)
@@ -112,6 +129,7 @@ Inhibits startup screen on the first unrecognised option."
 ;; Other Packages
 ;; ---------------------------------------------------------------------------
 (use-package org)
+(electric-indent-mode -1)
 
 ;; Elcord - For Discord Rich Presence
 (load "elcord")
